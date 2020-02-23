fc1f23f17c77fa56af2a47602255c913907cd009
Damon Davison <damon@curatur.com>
2020-02-23 17:22:45 -0500

    Add flatten/1 for a nicer API

--- lib/flattener.ex
+++ lib/flattener.ex
@@ -2,17 +2,16 @@ defmodule Flattener do
   @moduledoc "An implementation of a list flattener"
 
   @doc "Flatten a list"
-  def flatten([first | rest] = args, list) do
+  def flatten(list), do: flatten(list, [])
+
+  defp flatten([first | rest] = args, list) do
     flattened = flatten(first, list)
     flatten(rest, flattened)
   end
 
-  @doc "Handle no elements left"
-  def flatten([], list), do: list
+  defp flatten([], list), do: list
 
-  @doc "Handle a single element"
-  def flatten(single, list) when is_integer(single), do: list ++ [single]
+  defp flatten(single, list) when is_integer(single), do: list ++ [single]
 
-  @doc "Handle bad data"
-  def flatten(_, list), do: []
+  defp flatten(_, list), do: []
 end
